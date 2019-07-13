a3ce0ee0d80ac1a6fe8c151ae85dc30b6c009cde
Richard Le <richardle1997@gmail.com>
2019-07-13 12:31:43 -0700

    Set up Branching

--- app/src/main/java/com/example/canary/MapsActivity.java
+++ app/src/main/java/com/example/canary/MapsActivity.java
@@ -25,6 +25,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
 
+    //Comment to start the branching
+
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
