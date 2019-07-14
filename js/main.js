0d8b1651c46b90c11b7c00c4f8c1865091190305
Alexis C. Mendiola <alexis_cm@live.com>
2019-07-14 01:06:04 -0700

    Added JQuery click functions to btns

--- js/main.js
+++ js/main.js
@@ -6,4 +6,39 @@ ref.on("value", function(snap){
   });
 });
 
+let $sosButton = $("#sos-button");
+let $icons = $('.features-icons-item');
+let $cancelButton = $("#cancel-sos-btn");
+let iconDisplayNone = true;
+let sosDisplayNone = false;
 
+$sosButton.on("click", () => {
+  if (iconDisplayNone) {
+    for (i = 0; i < $icons.length; i++) {
+      $icons[i].className = "features-icons-item mx-auto";
+    }
+    iconDisplayNone = false;
+
+    /* Hides SOS button */
+    $sosButton[0].className += " d-none";
+    sosDisplayNone = true;
+
+    /* Reveals Cancel SOS button */
+    $cancelButton[0].className = "col-5 mx-auto text-center py-4 animated fadeInUp";
+  }
+});
+
+$cancelButton.on("click", () => {
+  if (sosDisplayNone) {
+    for (i = 0; i < $icons.length; i++) {
+      $icons[i].className = "col-5 mx-auto text-center py-4 animated fadeInUp d-none";
+    }
+    iconDisplayNone = true;
+
+    /* Hides Cancel SOS button */
+    $cancelButton[0].className += " d-none";
+
+    /* Reveals SOS request button */
+    $sosButton[0].className = "col-5 mx-auto text-center py-4 animated fadeInUp";
+  }
+});
